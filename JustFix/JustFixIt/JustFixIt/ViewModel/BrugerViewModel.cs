54891535f0e8bbb920aced73c72097a391ef6f35
LeSubmarine <henrik@osterby.dk>
2019-11-28 09:20:43 +0100

    Knaps

---
+++ JustFix/JustFixIt/JustFixIt/ViewModel/BrugerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NoteMVVM;
+
+namespace JustFixIt.ViewModel
+{
+    class BrugerViewModel
+    {
+        public ICommand BrugerBack { get; set; }
+        public BrugerViewModel()
+        {
+                BrugerBack = new RelayCommand(Back);
+        }
+
+        public void Back()
+        {
+        }
+    }
+}
