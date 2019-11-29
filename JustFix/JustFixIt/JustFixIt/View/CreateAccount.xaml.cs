48427af3df2a766ed4f093871b9a1f8185e8f797
Big Tobs <tobias@hillmann.dk>
2019-11-29 11:40:54 +0100

    sure thang

--- JustFix/JustFixIt/JustFixIt/View/CreateAccount.xaml.cs
+++ JustFix/JustFixIt/JustFixIt/View/CreateAccount.xaml.cs
@@ -26,5 +26,10 @@ namespace JustFixIt.View
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
