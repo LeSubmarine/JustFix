ac773fa76f0cc805fd9929e9b9eb4c6181bca1cd
LeSubmarine <henrik@osterby.dk>
2019-11-29 09:34:06 +0100

    Stuff

--- JustFix/JustFixIt/JustFixIt/ViewModel/MainViewModel.cs
+++ JustFix/JustFixIt/JustFixIt/ViewModel/MainViewModel.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JustFixIt.Model;
 
 namespace JustFixIt.ViewModel
 {
     class MainViewModel
     {
+
+        #region Properties
+        public List<User> AllUsers { get; set; }
+        #endregion
+
         public MainViewModel()
         {
 
