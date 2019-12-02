5575009f7b634ab20d76bc8cf15f81a01e8e4348
LeSubmarine <henrik@osterby.dk>
2019-12-02 01:18:24 +0100

    Task etc

---
+++ JustFix/JustFixIt/JustFixIt/Model/TaskTypes/BrakeFluidChangeTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustFixIt.Model.TaskTypes
+{
+    class BrakeFluidChangeTask : WorkTask
+    {
+        public BrakeFluidChangeTask(Car car) : base(car)
+        {
+            Price = Convert.ToInt32(0 * Car.PriceModifier());
+            Time = Convert.ToInt32(0 * Car.TimeModifier());
+        }
+    }
+}
---
+++ JustFix/JustFixIt/JustFixIt/Model/WorkTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustFixIt.Model
+{
+    class WorkTask
+    {
+        #region Constructor
+        public WorkTask(Car car)
+        {
+            Car = car;
+        }
+        #endregion
+
+
+        #region Properties
+        public int Time { get; set; }
+        public int Price { get; set; }
+        public Car Car { get; set; } 
+        #endregion
+    }
+}
