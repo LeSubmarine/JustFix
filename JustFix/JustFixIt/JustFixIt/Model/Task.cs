593b40373f9003415c8dca669f64ed0f56c5da16
NikolajHK <niko3009@edu.easj.dk>
2019-12-11 13:20:23 +0100

    stuff

---
+++ JustFix/JustFixIt/JustFixIt/Model/Task.cs
@@ -0,0 +1,32 @@
+﻿namespace JustFixIt.Model
+{
+    abstract class Task
+    {
+        #region Constants
+        public enum TaskTypes
+        {
+            ChangeTires,
+            ChangeBrakes,
+            Suspension,
+            ExhaustSystem,
+            OilLubricationSystem,
+            StarterSystem,
+            Engine,
+            Electronics,
+            Gearbox
+        }
+        #endregion
+
+        #region Properties
+        public int Time { get; set; }
+
+        public int Price { get; set; } 
+
+
+        #endregion
+
+
+
+
+    }
+}
